697f9acff99f0a4b586464dfe4f081f82cdcbbd3
oscarcillo <oscar.tutor.rubio@gmail.com>
2019-01-31 20:32:03 +0100

    Implementada opción para mostrar los datos de la BD de Firebase

--- app/src/main/java/com/otr/ejemplo_autenticacion_firebase/DatosActivity.java
+++ app/src/main/java/com/otr/ejemplo_autenticacion_firebase/DatosActivity.java
@@ -1,24 +1,38 @@
 package com.otr.ejemplo_autenticacion_firebase;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatosActivity extends AppCompatActivity {
 
     EditText editTextName;
     Button buttonAdd;
     Spinner spinnerGenres;
+    ListView listViewArtists;
+    ProgressBar progreso;
 
     DatabaseReference databaseArtists;
+
+    List<Artist> artistList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +42,38 @@ public class DatosActivity extends AppCompatActivity {
         editTextName = findViewById(R.id.editTextName);
         buttonAdd = findViewById(R.id.buttonAddArtist);
         spinnerGenres = findViewById(R.id.spinnerGenres);
+        listViewArtists = findViewById(R.id.listViewArtists);
+        progreso = findViewById(R.id.progressBar);
         //
         databaseArtists = FirebaseDatabase.getInstance().getReference("artists");
+        //
+        artistList = new ArrayList<>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        databaseArtists.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                artistList.clear();
+                progreso.setVisibility(View.VISIBLE);
+
+                for(DataSnapshot artistSnapshot: dataSnapshot.getChildren()){
+                    Artist artist = artistSnapshot.getValue(Artist.class);
+                    artistList.add(artist);
+                }
+                ArtistList adapter = new ArtistList(DatosActivity.this, artistList);
+                listViewArtists.setAdapter(adapter);
+                progreso.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                progreso.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 
     public void addArtist(View v){
